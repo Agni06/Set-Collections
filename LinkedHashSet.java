84faed092ba63571f5abd8d414a8b45d254e0c32
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-18 17:19:20 +0530

    Create LinkedHashSet.java
    Implemented LinkedHashSet .

---
+++ LinkedHashSet.java
@@ -0,0 +1,20 @@
+class App
+  {
+public static void main(String, args[])
+    {
+Set<String> s1 = new LinkedHashSet<String>();
+s1.add("Agni");
+s1.add("Charan");
+s1.add("Teja");
+
+      for(String str : s1)
+        {
+
+          System.out.println(str);
+          
+        }
+
+      
+    }
+    
+  }
