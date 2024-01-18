1a0e446ffc2a726cf52111c870538670c1bb7f76
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-18 17:14:15 +0530

    Update creatingSet.java

--- creatingSet.java
+++ creatingSet.java
@@ -4,8 +4,8 @@ public static void main(String, args[])
     {
 Set<String> s1 = new HashSet<String>();
 s1.add("Agni");
-s2.add("Charan");
-s2.add("Teja");
+s1.add("Charan");
+s1.add("Teja");
 
       System.out.println(s1);
       
