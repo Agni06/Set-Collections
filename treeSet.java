8876cf00b47a581cc273b5ffc48abde9cca4534b
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-18 17:14:49 +0530

    Create treeSet.java
    Implemented simple tree set

---
+++ treeSet.java
@@ -0,0 +1,16 @@
+class App
+  {
+public static void main(String, args[])
+    {
+
+Set<String> s1 = new TreeSet<String>();
+
+s1.add("Agni");
+s1.add("Charan");
+s1.add("Teja");
+
+      System.out.println(s1);
+      
+      
+    }
+  }
