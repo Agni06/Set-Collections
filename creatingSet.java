386946cff96e754d1f668f815d50488c8a7c462c
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-18 17:10:32 +0530

    Create creatingSet.java
    Implemented simple set collection of String.

---
+++ creatingSet.java
@@ -0,0 +1,14 @@
+class App
+  {
+public static void main(String, args[])
+    {
+Set<String> s1 = new HashSet<String>();
+s1.add("Agni");
+s2.add("Charan");
+s2.add("Teja");
+
+      System.out.println(s1);
+      
+      
+    }
+  }
